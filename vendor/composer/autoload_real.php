2f3ac2db893591afe3727d6e6304aade11cdbdeb
Jono Mingard <reason.koan@gmail.com>
2019-02-06 01:39:40 +0900

    Composer install and add composer.lock to make heroku buildpack happy

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -29,13 +29,22 @@ class ComposerAutoloaderInit3b463ea043ba6b29a86a9e45adc63324
 
             call_user_func(\Composer\Autoload\ComposerStaticInit3b463ea043ba6b29a86a9e45adc63324::getInitializer($loader));
         } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
             $classMap = require __DIR__ . '/autoload_classmap.php';
             if ($classMap) {
                 $loader->addClassMap($classMap);
             }
         }
 
-        $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
         if ($useStaticLoader) {
